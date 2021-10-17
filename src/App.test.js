9a667f6beb30325205396d27577193ae4abf9266
otaviobonassi <otavio.bonassi@gupy.com.br>
2021-10-16 23:44:51 -0300

    add unit testing and loading state

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import '@testing-library/jest-dom'
 import App from './App';
+import FIXTURE from './App.fixture';
 
-test('renders learn react link', () => {
+const server = setupServer(
+  rest.get('/59f08692310000b4130e9f71', (req, res, ctx) => {
+    return res(ctx.json(FIXTURE))
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+test('makes http request to get mandatory data', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,25 @@ import { EventInterface } from './interfaces/Event';
 import { EventsRequest } from './interfaces/Requests';
 import SideModalProvider from './contexts/SideModalContext';
 import MarketSelectionsProvider from './contexts/MarketsSelectionContext';
+import Loading from './assets/loading.gif';
 
 function App() {
   const [events, setEvents] = useState<EventInterface[]>([]);
 
   const getData = async () => {
-    const { data } = await api.get('/59f08692310000b4130e9f71') as EventsRequest;
+    const { data } = await api.get('/59f08692310000b4130e9f71/') as EventsRequest;
     setEvents(data);
   }
 
   useEffect(() => {
     getData();
   }, []);
+
+  if (!events.length) return (
+    <div className="loading">
+      <img src={Loading} alt="Loading..."/>
+    </div>
+  )
 
   return (
     <SideModalProvider>
